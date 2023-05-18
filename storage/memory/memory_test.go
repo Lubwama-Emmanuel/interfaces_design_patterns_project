0c97e4db594e21d80969a53f7d22eec78c2db04b
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-05-18 11:08:57 +0300

    test:(added memory test file)

--- storage/memory/memory_test.go
+++ storage/memory/memory_test.go
@@ -32,21 +32,67 @@ func TestMemory(t *testing.T) {
 			},
 			wantErr: assert.NoError,
 		},
+		{
+			testName: "Error",
+			args: args{
+				path: "2",
+				data: models.DataObject{
+					"0706039119": "Emmanuel",
+				},
+			},
+			wantErr: assert.Error,
+		},
 	}
 
 	for _, tc := range tests {
 		tc := tc
 		t.Run(tc.testName, func(t *testing.T) {
 			t.Parallel()
-			memoryDb := memory.NewMemoryStorage()
-			createErr := memoryDb.Create(tc.args.path, tc.args.data)
+
+			memoryDB := memory.NewMemoryStorage()
+
+			createErr := memoryDB.Create(tc.args.path, tc.args.data)
 			if createErr != nil && tc.wantErr == nil {
 				assert.Fail(t, fmt.Sprintf("Test %v Error not expected but got one:\n"+"error: %q", tc.testName, createErr))
 				return
 			}
 
-			memoryDb.Read(tc.args.path)
+			data, readErr := memoryDB.Read(tc.args.path)
+			if readErr != nil && tc.wantErr == nil {
+				assert.Fail(t, fmt.Sprintf("Test %v Error not expected but got one:\n"+"error: %q", tc.testName, readErr))
+				return
+			}
+
+			if !compareMaps(data, tc.args.data) {
+				assert.Fail(t, fmt.Sprintf("Test %v data received %v yet expected %v", tc.testName, tc.args.data, data))
+			}
+
+			updateData := models.DataObject{
+				"0706039119": "Lubwama",
+			}
+			updateErr := memoryDB.Update(tc.args.path, updateData)
+			if updateErr != nil && tc.wantErr == nil {
+				assert.Fail(t, fmt.Sprintf("Test %v Error not expected but got one:\n"+"error: %q", tc.testName, updateErr))
+				return
+			}
 		})
 	}
+}
 
+func compareMaps(map1, map2 map[string]string) bool {
+	if len(map1) != len(map2) {
+		return false
+	}
+
+	for key, value1 := range map1 {
+		if value2, ok := map2[key]; ok {
+			if value1 != value2 {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+
+	return true
 }
