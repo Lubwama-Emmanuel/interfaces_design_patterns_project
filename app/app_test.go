dd4859badb965a46ba901c04d7351a8f68f01255
Rex <lubwamaemmanuel1@gmail.com>
2023-06-20 18:17:01 +0300

    test:(added test cases for delete and readall funcs)

--- app/app_test.go
+++ app/app_test.go
@@ -37,7 +37,13 @@ func TestApp(t *testing.T) {
 
 				f.storage.EXPECT().Read("a").Return(models.DataObject{"0704660968": "Emmanuel"}, nil)
 
-				f.storage.EXPECT().Update("a", models.DataObject{"0704660968": "Emmanuel"}).Return(nil).AnyTimes()
+				f.storage.EXPECT().Update("a", models.DataObject{"Emmanuel": "0704660968"}).Return(nil).AnyTimes()
+
+				f.storage.EXPECT().Delete("0704660968").Return(nil).AnyTimes()
+
+				f.storage.EXPECT().ReadAll().Return([]models.DataObject{
+					{"0704660968": "Emmanuel"},
+				}, nil)
 			},
 			args: args{
 				name:  "Emmanuel",
@@ -53,6 +59,10 @@ func TestApp(t *testing.T) {
 				f.storage.EXPECT().Read(gomock.Any()).Return(models.DataObject{}, assert.AnError)
 
 				f.storage.EXPECT().Update(gomock.Any(), gomock.Any()).Return(assert.AnError)
+
+				f.storage.EXPECT().Delete(gomock.Any()).Return(assert.AnError)
+
+				f.storage.EXPECT().ReadAll().Return([]models.DataObject{}, assert.AnError)
 			},
 			args: args{
 				name:  "Emmanuel",
@@ -94,6 +104,18 @@ func TestApp(t *testing.T) {
 			}
 
 			err = appInstance.UpdateName(tc.args.name, tc.args.phone)
+			if err != nil && tc.wantErr == nil {
+				assert.Fail(t, fmt.Sprintf("Test %v Error not expected but got one:\n"+"error: %q", tc.testName, err))
+				return
+			}
+
+			err = appInstance.DeleteContact(tc.args.phone)
+			if err != nil && tc.wantErr == nil {
+				assert.Fail(t, fmt.Sprintf("Test %v Error not expected but got one:\n"+"error: %q", tc.testName, err))
+				return
+			}
+
+			_, err =appInstance.GetAllPhoneNumbers()
 			if err != nil && tc.wantErr == nil {
 				assert.Fail(t, fmt.Sprintf("Test %v Error not expected but got one:\n"+"error: %q", tc.testName, err))
 				return
