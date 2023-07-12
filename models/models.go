affce37151c9b0e30278c6574c4e34e43db9fc60
Nickson <nicksonmjk@gmail.com>
2023-07-12 11:35:37 +0300

    chore: example env variable

--- models/models.go
+++ models/models.go
@@ -2,9 +2,7 @@ package models
 
 import "errors"
 
-var (
-	ErrNotFound = errors.New("models: resource not found")
-)
+var ErrNotFound = errors.New("models: resource not found")
 
 type DataObject map[string]string
 
