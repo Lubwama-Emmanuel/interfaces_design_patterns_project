ae2fac163b77ece7476a2a6c1202d44c74e5a318
Rex <lubwamaemmanuel1@gmail.com>
2023-07-18 23:20:40 +0300

    feat:(added connection file)

---
+++ storage/mongodb/connection.go
@@ -0,0 +1,45 @@
+package mongodb
+
+import (
+	"context"
+
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type mongoDB struct {
+	*mongo.Collection
+}
+
+func connectToDB(url string) *mongo.Client {
+	// Set client options
+	clientOptions := options.Client().ApplyURI(url)
+
+	// Connect to mongodb
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		return nil
+	}
+
+	// Ping the MongoDB server to check the connection
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		return nil
+	}
+
+	log.Info("----DATABASE CONNECTED SUCCESSFULLY")
+
+	return client
+}
+
+func NewMongoDB(url string) *mongoDB {
+	client := connectToDB(url)
+
+	// Access the database and collection
+	database := client.Database("test_contacts")
+	collection := database.Collection("contacts")
+	return &mongoDB{
+		Collection: collection,
+	}
+}
